e967957e78cd4b0121943df3b8b67be9457f87fa
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-01-15 11:34:31 +0100

    removed useless  import

--- src/test/java/it/unibo/mapComponents/TestRoom.java
+++ src/test/java/it/unibo/mapComponents/TestRoom.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
