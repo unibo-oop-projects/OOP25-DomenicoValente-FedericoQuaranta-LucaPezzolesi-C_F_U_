8fd2afdc2824d3b3d28a164e2386773e5d5caa2f
Federico Quaranta <federico.quaranta2@studio.unibo.it>
2026-01-30 10:32:56 +0100

    finish TestPlayer: init, testInitialization, testMovement, testScore, testInventoryInteractions

--- src/test/java/it/unibo/player/TestPlayer.java
+++ src/test/java/it/unibo/player/TestPlayer.java
@@ -3,7 +3,10 @@ package it.unibo.player;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,8 +15,6 @@ import it.unibo.api.Position;
 import it.unibo.api.doors.Door;
 import it.unibo.api.player.Player;
 import it.unibo.api.key.Key;
-import it.unibo.impl.DoorImpl;
-import it.unibo.impl.Inventory;
 import it.unibo.impl.PlayerImpl;
 import it.unibo.impl.templates.KeyTemplate;
 
@@ -28,7 +29,7 @@ public class TestPlayer{
     }
 
     @Test
-    void testInitiaziation(){
+    void testInitialization(){
         assertEquals(this.startPosition, this.player.getPosition());
         assertEquals(0,this.player.getPoints());
         assertNotNull(this.player.getInventory());
@@ -58,6 +59,13 @@ public class TestPlayer{
     void testInventoryInteractions(){
         assertNotNull(this.player.getInventory());
 
-        
+        Door dummyDoor = mock(Door.class);
+        Key newKey = new KeyTemplate("key-001", "first-door", dummyDoor);
+
+        this.player.addKeyToInventory(Optional.of(newKey));
+        assertNotNull(this.player.getInventory());
+
+        assertTrue(this.player.getInventory().hasTheKey(newKey.getId()));
+
     }
 }
