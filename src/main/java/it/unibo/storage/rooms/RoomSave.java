30f164bfddd872bde182c29b5133604d8ddab7f3
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-02-13 12:44:16 +0100

    changed file path to work with a single jar file

--- src/main/java/it/unibo/storage/rooms/RoomSave.java
+++ src/main/java/it/unibo/storage/rooms/RoomSave.java
@@ -105,7 +105,7 @@ public class RoomSave {
         final TagInspector tagInsp = t -> t.getClassName().startsWith("it.unibo");
         loadOpt.setTagInspector(tagInsp);
 
-        try(final InputStream fis = new FileInputStream(GameSettings.ROOM_YAML_FILES_DEFAULTPATH.getValue())) {
+        try(final InputStream fis = RoomSave.class.getResourceAsStream("/rooms.yml")) {
             final Yaml yamlRead = new Yaml(new Constructor(List.class, loadOpt));
             final List<DataForRooms> rawData = yamlRead.load(fis);
 
